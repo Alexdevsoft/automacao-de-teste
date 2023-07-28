29dbfcfa87720ad2cd746770e94887a422c1cb53
Alexdevsoft <alexhavilla2022@gmail.com>
2023-07-27 22:36:56 -0300

    Second commit

---
+++ src/test/java/automatizado/page/GooglePO.java
@@ -0,0 +1,16 @@
+package automatizado.page;
+
+import org.openqa.selenium.WebDriver;
+
+public class GooglePO extends BasePO{
+
+    /**
+     * Construtor para criação para a página do Google
+     * @param dirver Driver da página do Google
+     */
+    protected GooglePO(WebDriver driver) {
+        super(driver);
+        
+    }
+    
+}
