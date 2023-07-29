1c87ba4dcd34ebc7a7167ebdbdfe4fd3a2ce78db
Alexdevsoft <alexhavilla2022@gmail.com>
2023-07-28 23:55:14 -0300

    terceiro commit

--- src/test/java/automatizado/page/GooglePO.java
+++ src/test/java/automatizado/page/GooglePO.java
@@ -1,16 +1,41 @@
 package automatizado.page;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class GooglePO extends BasePO{
+
+    @FindBy(name = "q")
+    public WebElement inputPesquisa;
+
+    @FindBy(id = "result-stats")
+    public WebElement divResultadoPesquisa;
 
     /**
      * Construtor para criação para a página do Google
      * @param dirver Driver da página do Google
      */
-    protected GooglePO(WebDriver driver) {
+    public GooglePO(WebDriver driver) {
         super(driver);
         
+    }
+
+    /**
+     * Metodo que irá efetuar uma pesquisa no Google baseando no texto no informando e concluíndo com entener
+     * @param texto Texto a ser pesquisado
+     */
+    public void pesquisar(String texto){
+        inputPesquisa.sendKeys(texto + Keys.ENTER);
+    }
+
+    /**
+     * Método que retorna o resultado aproximado da pesquisa
+     * @return Retorna o resultado na pesquisa.
+     */
+    public String resultadoDaPesquisa(){
+        return divResultadoPesquisa.getText();
     }
     
 }
--- src/test/java/automatizado/test/GoogleTest.java
+++ src/test/java/automatizado/test/GoogleTest.java
@@ -2,24 +2,47 @@ package automatizado.test;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
+
+import automatizado.page.GooglePO;
 
 public class GoogleTest extends BaseTest{
+
+
+    private static GooglePO googlePage;
+
+    @BeforeClass
+    public static void prepararTest(){
+        googlePage = new GooglePO(driver);
+    }
     
 
     @Test
-    public void testarNoGoogle(){
+    public void TC001_devePesquisarNoGoogleBatataFrita(){
 
-        WebElement inputPesquisa = driver.findElement(By.name("q"));
-        inputPesquisa.sendKeys("Batata frita" + Keys.ENTER);
+        //WebElement inputPesquisa = driver.findElement(By.name("q"));
 
-        String resultado = driver.findElement(By.id("result-stats")).getText();
+        //googlePage.inputPesquisa.sendKeys("Batata frita" + Keys.ENTER);
+        googlePage.pesquisar("Batata frita");
+
+        String resultado = googlePage.resultadoDaPesquisa();
 
         assertTrue(resultado, resultado.contains("Aproximadamente"));
                 
     }
     
+    @Test
+    public void TC002_devePesquisarNoGooglePizza(){
+
+        //WebElement inputPesquisa = driver.findElement(By.name("q"));
+
+        googlePage.pesquisar("Pizza");
+
+        String resultado = googlePage.resultadoDaPesquisa();
+
+        assertTrue(resultado, resultado.contains("resultados"));
+                
+    }
 }
